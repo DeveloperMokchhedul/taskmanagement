cc182e40e73d115da12cda0a24ec7872576a0598
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-01-04 14:55:55 +0600

    task form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import TaskForm from "./components/Taskform"
 
 
 
 function App() {
   return (
     <>
-      <div className="w-1/2 bg-black/25 mx-auto my-[120px]">
-        <h1 className="text-red-600 text-3xl">  task management project</h1>
+      <div className="w-1/2 bg-black/25 mx-auto my-[120px] p-[10px] rounded-lg">
+      <h1 className="uppercase text-center font-bold text-2xl">Task Management Project</h1>
+      <div>
+        <TaskForm />
+      </div>
 
-
+      
       </div>
     </>
   )
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,32 +1,30 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// interface TaskFormProps {
-//   addTask: (title: string) => void;
-// }
 
-// const TaskForm: React.FC<TaskFormProps> = ({ addTask }) => {
-//   const [taskTitle, setTaskTitle] = useState("");
+const TaskForm: React.FC = () => {
+  const [taskTitle, setTaskTitle] = useState("");
 
-//   const handleSubmit = (e: React.FormEvent) => {
-//     e.preventDefault();
-//     if (taskTitle.trim()) {
-//       addTask(taskTitle);
-//       setTaskTitle("");
-//     }
-//   };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(`task title is ${taskTitle}`);
+    
 
-//   return (
-//     <form onSubmit={handleSubmit} className="task-form">
-//       <input
-//         type="text"
-//         value={taskTitle}
-//         onChange={(e) => setTaskTitle(e.target.value)}
-//         placeholder="Add a new task"
-//         className="input"
-//       />
-//       <button type="submit" className="btn">Add</button>
-//     </form>
-//   );
-// };
+  };
 
-// export default TaskForm;
+  return (
+    <form onSubmit={handleSubmit} className="task-form">
+    <div className="flex gap-5">
+    <input 
+        type="text"
+        value={taskTitle}
+        onChange={(e) => setTaskTitle(e.target.value)}
+        placeholder="Add a new task"
+        className="w-full bg-transparent px-3 py-2 rounded-lg outline-none border border-green-600"
+      />
+      <button type="submit" className="bg-green-600 px-5 rounded-lg">Add</button>
+    </div>
+    </form>
+  );
+};
+
+export default TaskForm;
