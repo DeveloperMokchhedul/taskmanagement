de8b39c1e9803dd511e9a8a12c08dd1b25906ffa
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-01-05 09:57:52 +0600

    edit and delete button added

--- src/components/ShowTask.tsx
+++ src/components/ShowTask.tsx
@@ -4,11 +4,18 @@ function ShowTask({allTask}) {
     console.log(`all task is ${allTask}`);
     
   return (
-    <div>
+    <div className="my-[40px]">
         {
-            allTask.map((task)=>(
-                <div>
-                    <h1>{task.title}</h1>
+            allTask &&  allTask.map((data)=>(
+                <div key={data.id} className="w-[80%] mx-auto mt-[10px]  flex justify-between gap-[100px]">
+                    <div>
+
+                    <h1>{data.title}</h1>
+                    </div>
+                    <div className="flex gap-[10px]">
+                        <button className="bg-green-600 px-[20px] rounded-lg py-[5px]">Edit</button>
+                        <button className="bg-red-600 px-[20px] rounded-lg py-[5px]">Delete</button>
+                    </div>
                 </div>
             ))
         }
